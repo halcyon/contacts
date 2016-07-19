4158f3f334a277eac42b47663a1043fb4786de71
Scott McLeod <halcyonblue@gmail.com>
2016-07-19 17:29:45 -0400

    Refactoring

--- src/contacts/core.clj
+++ src/contacts/core.clj
@@ -15,36 +15,26 @@
    :access-token-uri "https://accounts.google.com/o/oauth2/token"
    :scope "profile https://www.googleapis.com/auth/contacts"})
 
-(defn authorize-uri [client-params csrf-token]
-  (str
-   (:authorize-uri client-params)
-   "?response_type=code"
-   "&client_id="
-   (url-encode (:client-id client-params))
-   "&redirect_uri="
-   (url-encode (:redirect-uri client-params))
-   "&scope="
-   (url-encode (:scope client-params))
-   "&state="
-   (url-encode csrf-token)))
+(defn authorize-uri
+  []
+  (str (:authorize-uri oauth2-params)
+       "?response_type=code"
+       "&client_id=" (url-encode (:client-id oauth2-params))
+       "&redirect_uri=" (url-encode (:redirect-uri oauth2-params))
+       "&scope=" (url-encode (:scope oauth2-params))
+       ;; "&state=" (url-encode csrf-token)
+       ))
 
-(defn get-authentication-response [csrf-token response-params]
-  (when (= csrf-token (:state response-params))
-    (try
-      (:body (http/post (:access-token-uri oauth2-params)
-                        {:form-params {:code         (:code response-params)
-                                       :grant_type   "authorization_code"
-                                       :client_id    (:client-id oauth2-params)
-                                       :redirect_uri (:redirect-uri oauth2-params)}
-                         :basic-auth [(:client-id oauth2-params) (:client-secret oauth2-params)]
-                         :as          :json}))
-      (catch Exception _ nil))))
-
-
-(authorize-uri oauth2-params "bob")
-(defn auth
-  [code]
-  (let [response (json/parse-string (get-authentication-response "bob" {:state "bob"
-                                                                        :code code})
-                                    true)]
-    (swap! auth-db assoc :response response)))
+(defn update-access-token!
+  [authorization-code]
+  (let [authorization-resp (try
+                             (http/post (:access-token-uri oauth2-params)
+                                        {:form-params {:code         authorization-code
+                                                       :grant_type   "authorization_code"
+                                                       :client_id    (:client-id oauth2-params)
+                                                       :redirect_uri (:redirect-uri oauth2-params)}
+                                         :basic-auth  [(:client-id oauth2-params) (:client-secret oauth2-params)]})
+                             (catch Exception _ nil))
+        grant-resp         (json/parse-string (:body authorization-resp)
+                                              true)]
+    (swap! auth-db assoc :grant grant-resp)))
